d239db7d78260359fab38a83b47a49b21e9e97ee
Sony Arouje <sonyarouje@gmail.com>
2017-09-22 18:47:24 +0530

    adding some comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,9 @@
 /* eslint fp/no-unused-expression: 0 */
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-// import { createStore } from 'redux';
 import store from './store/create-store';
-// import reducers from './reducers';
 import App from './App';
-
-// const storeEnhancer = window.devToolsExtension ? window.devToolsExtension() : value => value;
-// const store = createStore(reducers, storeEnhancer);
 
 render(
   <Provider store={store}>
--- src/store/create-store.js
+++ src/store/create-store.js
@@ -6,13 +6,18 @@ import { makeHTTPDriver } from '@cycle/http';
 import { timeDriver } from '@cycle/time';
 import logger from 'redux-logger';
 
+// combined all the reducers in the application
 import reducers from './reducers';
+// combined all the cycles in the application
 import cycles from './cycles';
 
-
+// create cycle middleware to attach to redux store.
 const cycleMiddleware = createCycleMiddleware();
 const { makeActionDriver, makeStateDriver } = cycleMiddleware;
 
+// we might use multiple middleware, here i used a logger
+// and cycle. We can add more middleware by adding them to
+// the below array.
 const middleWare = [
   logger,
   cycleMiddleware,
@@ -20,8 +25,11 @@ const middleWare = [
 
 const initState = {};
 
+// more about compose here http://redux.js.org/docs/api/compose.html
 let composeEnhancers = compose;// eslint-disable-line
 
+// adding redux dev tool to visualize the store state.
+// should be enabled only in development.
 if (process.env.NODE_ENV !== 'production') { // eslint-disable-line
   const composeWithDevToolsExtension =
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // eslint-disable-line no-underscore-dangle
@@ -31,13 +39,15 @@ if (process.env.NODE_ENV !== 'production') { // eslint-disable-line
 }
 
 const store = createStore(
-    reducers,
-    initState,
+    reducers, // all the available reducers combined
+    initState, // initial state of the reducers
     composeEnhancers(
-     applyMiddleware(...middleWare),
+     applyMiddleware(...middleWare), // attaching the middleware
     ),
   );
 
+// calling run of cycle we are activating the cycles that we created
+// here all the different cycles are combined to one.
 run(cycles, {
   ACTION: makeActionDriver(),
   STATE: makeStateDriver(),
